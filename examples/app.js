f40e1de6330b56dc902f79ba833f60bf6ce3b7d6
Mateusz <mateusz@qunabu.com>
2018-04-09 09:03:19 +0200

    init commit

--- examples/app.js
+++ examples/app.js
@@ -3,12 +3,14 @@ import Grid from "./../src/grid";
 
 const config = require('sass-extract-loader!./_bootstrap-config.scss');
 
-
 let grid = new Grid(config);
-grid.attach();
-grid.toggle();
+grid.attach(); // attaches grid to DOM
+grid.toggle(); // show/hide grid helper 
 
-console.log(config, grid.config);
+console.log(grid.columns);
+console.log(grid.containerWidth);
+console.log(grid.gutterWidth);
+console.log(grid.columnWidth);
 
 
-window.__grid = grid;
+window.__grid = grid; // this is just for test purposes
